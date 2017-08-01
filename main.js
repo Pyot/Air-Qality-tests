e979c6780cdcefbf458d3dccd263d6ddfeda06d3
Pyot <piotr.a.ludwig@gmail.com>
2017-08-01 10:04:01 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+document.ready() {
+     var url = 'https://api.waqi.info/feed/' + town + '/?token=4e9a9b9811fab51ec451adc577b177e3fef457b1';
+               
+
+                $.getJSON(url, function (pollution) {
+                    
+                    
+                };
+                          
+                          
+    
+};
